e0829e17c3d421f79cb2a62b9361209931583ea4
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-09 22:37:58 +0530

    Add:File

---
+++ ProgrammingConstruct-Day2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgrammingConstruct_Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....Programing Construct......!");
+        }
+    }
+}
