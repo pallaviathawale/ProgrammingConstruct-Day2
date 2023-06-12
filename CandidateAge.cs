101e0168f2550d2572488046cecc6df96a7a4974
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-12 14:18:19 +0530

    Add: UC3-CandidateAge

---
+++ CandidateAge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct_Day2
+{
+    internal class CandidateAge
+    {
+        public void VoteCast()
+        {
+            int age;
+            Console.WriteLine("Enter Age of Candidate to check...");
+            age = Convert.ToInt32(Console.ReadLine());
+            if(age >=18)
+            {
+                Console.WriteLine("Candidate is eligibile for casting his/her vote");
+
+            }
+            else
+            {
+                Console.WriteLine("Candidate is not elibile for casting his/her vote");
+
+            }
+        }
+    }
+}
